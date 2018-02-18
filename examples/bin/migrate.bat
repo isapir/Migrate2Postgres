b81f16fe6ffc592f00719c30385c860b76f32e26
Igal Sapir <dev@21solutions.net>
2018-02-18 13:13:54 -0800

    improved example bin scripts

--- examples/bin/migrate.bat
+++ examples/bin/migrate.bat
@@ -1,15 +1,14 @@
 
 set M2PG_CONF=MsSqlAWT2Postgres.conf
 
-set M2PG_COMMAND=DDL
-
 set M2PG_PROP=
-set M2PG_PROP=%M2PG_PROP% -Dconnections.mssql.user=sa
+set M2PG_PROP=%M2PG_PROP% -Dconnections.mssql.user=readonly
 set M2PG_PROP=%M2PG_PROP% -Dconnections.mssql.password=secret
-set M2PG_PROP=%M2PG_PROP% -Dconnections.pg.user=postgres
-set M2PG_PROP=%M2PG_PROP% -Dconnections.pg.password=
 
-set M2PG_CLASSPATH="lib/*;lib/drivers/*"
+set M2PG_CLASSPATH="lib/*"
+
+:: get the command from the first arg passed, must be either DDL or DML
+set M2PG_COMMAND=%1
 
 
 java %M2PG_PROP% -cp %M2PG_CLASSPATH% net.twentyonesolutions.m2pg.PgMigrator %M2PG_COMMAND% %M2PG_CONF%
--- examples/bin/migrate.sh
+++ examples/bin/migrate.sh
@@ -1,15 +1,14 @@
 
 M2PG_CONF=MsSqlAWT2Postgres.conf
 
-M2PG_COMMAND=DDL
-
 M2PG_PROP=
-M2PG_PROP=%M2PG_PROP% -Dconnections.mssql.user=sa
+M2PG_PROP=%M2PG_PROP% -Dconnections.mssql.user=readonly
 M2PG_PROP=%M2PG_PROP% -Dconnections.mssql.password=secret
-M2PG_PROP=%M2PG_PROP% -Dconnections.pg.user=postgres
-M2PG_PROP=%M2PG_PROP% -Dconnections.pg.password=
 
-M2PG_CLASSPATH="lib/*:lib/drivers/*"
+M2PG_CLASSPATH="lib/*"
+
+## get the command from the first arg passed, must be either DDL or DML
+M2PG_COMMAND=$1
 
 
 java $M2PG_PROP -cp $M2PG_CLASSPATH net.twentyonesolutions.m2pg.PgMigrator $M2PG_COMMAND $M2PG_CONF
